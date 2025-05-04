942aa6748484e07bb1d21d7cfdc1b5c0c3bfd44f
marcocofano <m.cofano@gmail.com>
2025-05-04 19:56:43 +0200

    Move slides to decks/ included public/ folder

--- slides/vite.config.ts
+++ slides/vite.config.ts
@@ -5,8 +5,8 @@ import { resolve } from 'path'
 export default defineConfig({
   resolve: {
     alias: {
-      '@master_bari': resolve(__dirname, './decks/master_bari'),
-      '@img': resolve(__dirname, './public'),
+      '@slides': resolve(__dirname, './decks'),
+      '@img': resolve(__dirname, './decks/public'),
     },
   },
 })
